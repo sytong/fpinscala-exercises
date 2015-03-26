a0ce8d9e27ee6a366d1c32e6475cdaa886951aa8
sytong <tongsinyin@gmail.com>
2015-03-26 18:42:02 +0800

    Check in exercise 2.1

---
+++ chapter2/exercise2.1/Fibonacci.scala
@@ -0,0 +1,37 @@
+object Fibonacci {
+  def fib_non_tailrec(n: Int): Int = {
+    def go(n: Int): Int =
+      if (n == 0) 0
+      else if (n == 1) 1
+      else go(n-1) + go(n-2)
+
+    go(n)
+  }
+
+  def fib(n: Int): Int = {
+  	@annotation.tailrec
+  	def go(n1: Int, n2: Int, i: Int): Int =
+ 	  if (i > n) n1+n2
+  	  else if (i == 0) go(0, 0, i+1)
+  	  else if (i == 1) go(0, 1, i+1)
+  	  else go(n1+n2, n1, i+1)
+
+  	go(0, 0, 0) 
+  }
+
+  private def formatFib(x: Int) = {
+    val msg = "The %d-th Fibonacci number is %d."
+    msg.format(x, fib(x))
+  }
+
+  private def test() {
+  	var x = 0
+  	for (x <- 0 to 11) {
+      assert(fib_non_tailrec(x) == fib(x), "Calculation of Fibonacci(" + x + ") failed.")
+  	}
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatFib(args(0).toInt))
+  }
+}
