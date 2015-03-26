168b97cbd79509743b14402a5d2a36818c809a41
sytong <tongsinyin@gmail.com>
2015-03-26 18:54:24 +0800

    Add some documentations

--- chapter2/exercise2.1/Fibonacci.scala
+++ chapter2/exercise2.1/Fibonacci.scala
@@ -1,4 +1,5 @@
 object Fibonacci {
+  // The non tail-recursive implementation
   def fib_non_tailrec(n: Int): Int = {
     def go(n: Int): Int =
       if (n == 0) 0
@@ -8,6 +9,7 @@ object Fibonacci {
     go(n)
   }
 
+  // The tail-recursive implementation
   def fib(n: Int): Int = {
     @annotation.tailrec
     def go(n1: Int, n2: Int, i: Int): Int =
@@ -19,11 +21,13 @@ object Fibonacci {
     go(0, 0, 0) 
   }
 
+  // Pretty print!
   private def formatFib(x: Int) = {
     val msg = "The %d-th Fibonacci number is %d."
     msg.format(x, fib(x))
   }
 
+  // I have no idea if it is the right way to do so...
   private def test() {
     var x = 0
     for (x <- 0 to 11) {
